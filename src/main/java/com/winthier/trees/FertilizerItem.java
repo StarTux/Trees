db0fb10212e076137ec843c44743eeb45687b7a3
StarTux <ths.loy@googlemail.com>
2017-04-26 11:51:22 +0200

    More effects

--- src/main/java/com/winthier/trees/FertilizerItem.java
+++ src/main/java/com/winthier/trees/FertilizerItem.java
@@ -8,7 +8,11 @@ import com.winthier.generic_events.GenericEventsPlugin;
 import com.winthier.generic_events.ItemNameEvent;
 import lombok.Getter;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -51,16 +55,27 @@ public final class FertilizerItem implements CustomItem, UncraftableItem {
     public void onPlayerInteract(PlayerInteractEvent event, ItemContext context) {
         if (!event.hasBlock()) return;
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-        TreeType treeType = TreeType.of(event.getClickedBlock());
-        if (treeType == null) return;
+        Block block = event.getClickedBlock();
+        TreeType treeType = TreeType.of(block);
+        Location loc = block.getLocation();
+        if (treeType == null) {
+            block.getWorld().spawnParticle(Particle.TOTEM, block.getRelative(event.getBlockFace()).getLocation(loc).add(0.5, 0.5, 0.5), 4, 0.25, 0.25, 0.25, 0);
+            block.getWorld().playSound(block.getLocation(loc), Sound.ENTITY_SLIME_ATTACK, SoundCategory.BLOCKS, 2.0f, 0.5f);
+            return;
+        }
         event.setCancelled(true);
         Player player = context.getPlayer();
-        Block block = event.getClickedBlock();
         if (!GenericEventsPlugin.getInstance().playerCanBuild(player, block)) return;
         Tree tree = plugin.getRandomTree(treeType);
         if (tree == null) return;
-        if (tree.isBlocked(block)) return;
+        if (tree.isBlocked(block)) {
+            block.getWorld().spawnParticle(Particle.TOTEM, block.getLocation(loc).add(0.5, 0.5, 0.5), 4, 0.25, 0.25, 0.25, 0);
+            block.getWorld().playSound(block.getLocation(loc), Sound.ENTITY_SLIME_DEATH, SoundCategory.BLOCKS, 2.0f, 0.5f);
+            return;
+        }
         tree.growSlowly(plugin, player, block);
+        block.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, block.getLocation(loc).add(0.5, 0.5, 0.5), 8, 0.25, 0.25, 0.25, 0);
+        block.getWorld().playSound(block.getLocation(loc), Sound.ENTITY_SLIME_JUMP, SoundCategory.BLOCKS, 2.0f, 1.5f);
         if (player.getGameMode() != GameMode.CREATIVE) {
             ItemStack item = context.getItemStack();
             item.setAmount(item.getAmount() - 1);
