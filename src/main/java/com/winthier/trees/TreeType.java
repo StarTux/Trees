e03afc11798be1d2e0bbed50f31da0a7284624ad
StarTux <ths.loy@googlemail.com>
2017-04-29 14:27:56 +0200

    All flowers and tall grass are saplings

--- src/main/java/com/winthier/trees/TreeType.java
+++ src/main/java/com/winthier/trees/TreeType.java
@@ -15,7 +15,17 @@ public enum TreeType {
     BROWN_MUSHROOM,
     RED_MUSHROOM,
     DEAD_BUSH,
-    BLUE_ORCHID;
+    TALL_GRASS,
+    FERN,
+    POPPY,
+    BLUE_ORCHID,
+    ALLIUM,
+    AZURE_BLUET,
+    RED_TULIP,
+    ORANGE_TULIP,
+    WHITE_TULIP,
+    PINK_TULIP,
+    OXEYE_DAISY;
 
     static TreeType of(TreeSpecies species) {
         if (species == null) throw new NullPointerException();
@@ -48,9 +58,25 @@ public enum TreeType {
             case 0: return DEAD_BUSH;
             default: return null;
             }
+        case LONG_GRASS:
+            switch (block.getData()) {
+            case 0: return DEAD_BUSH;
+            case 1: return TALL_GRASS;
+            case 2: return FERN;
+            default: return null;
+            }
         case RED_ROSE:
             switch (block.getData()) {
+            case 0: return POPPY;
             case 1: return BLUE_ORCHID;
+            case 2: return ALLIUM;
+            case 3: return AZURE_BLUET;
+            case 4: return RED_TULIP;
+            case 5: return ORANGE_TULIP;
+            case 6: return WHITE_TULIP;
+            case 7: return PINK_TULIP;
+            case 8: return OXEYE_DAISY;
+            default: return null;
             }
         default:
             return null;
